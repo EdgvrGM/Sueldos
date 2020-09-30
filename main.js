2879593ea1d28f665371f2054ff5d8a740d5eff6
Edgar Gonzalez <edgar_gm6@hotmail.com>
2020-09-29 23:09:44 -0500

    aumentos v1

---
+++ main.js
@@ -0,0 +1,24 @@
+// Cambiar la cantidad de la variable sueldo para probar todos los aumentos
+var sueldo = Number(prompt('INTRODUCE UN SUELDO'));
+var porcentaje;
+
+console.log(`Sueldo anterior: $${sueldo}`);
+
+if (sueldo > 0 && sueldo < 1000) {
+  porcentaje = 0.15;
+  sueldo = sueldo + sueldo * porcentaje;
+} else if (sueldo > 1000 && sueldo < 1200) {
+  porcentaje = 0.12;
+  sueldo = sueldo + sueldo * porcentaje;
+} else if (sueldo > 1200 && sueldo < 1400) {
+  porcentaje = 0.1;
+  sueldo = sueldo + sueldo * porcentaje;
+} else if (sueldo > 1400 && sueldo < 1500) {
+  porcentaje = 0.08;
+  sueldo = sueldo + sueldo * porcentaje;
+} else if (sueldo > 1500) {
+  porcentaje = 0.05;
+  sueldo = sueldo + sueldo * porcentaje;
+}
+
+console.log(`Su nuevo sueldo es de: $${sueldo}`);
